d2ead09a5ecaa2893b262ef0ce4aedf5efca80c0
aleendis <123junlee@naver.com>
2023-10-15 18:39:01 +0900

    request

--- Scan/Main.js
+++ Scan/Main.js
@@ -27,7 +27,7 @@ function Start({navigation}) {
   return (
     <View>
       <Text>Start!</Text>
-      <Button title = "request permissions" onPress={requestCameraPermission} />
+      <Button title = "request permission" onPress={requestCameraPermission} />
     </View>
   );
 }
