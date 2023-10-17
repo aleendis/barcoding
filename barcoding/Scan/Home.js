4d4259a24505ed1ea937b48e481e5fa32eae1e4d
aleendis <43637273+aleendis@users.noreply.github.com>
2023-10-17 18:26:40 +0900

    Scan success

---
+++ barcoding/Scan/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Button
+        title="Scan"
+        onPress={() => navigation.navigate('Scanner')}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ barcoding/Scan/Scan.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <View style={styles.scanAgain}>
+          <Text>Scan again?</Text>
+          <Text style={styles.scanAgainText} onPress={() => setScanned(false)}>
+            Tap Here
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  scanAgain: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 15,
+  },
+  scanAgainText: {
+    marginLeft: 10,
+    color: 'blue',
+  },
+});
