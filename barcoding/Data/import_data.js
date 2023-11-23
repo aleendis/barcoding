2268c3b972d29578f6324fb0c23a1b360f0c1b73
aleendis <43637273+aleendis@users.noreply.github.com>
2023-11-23 23:23:01 +0900

    bottomBar_Complete

--- barcoding/Data/import_data.js
+++ barcoding/Data/import_data.js
@@ -1,31 +1,67 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-let scannedData = '';
+function Import({ route }) {
+  const { data } = route.params;
+  const {prnm} = route.prnm;
+  const {deadline} = route.deadline;
+  const [productNumber, setProductNumber] = useState(data);
+  const [productName, setProductName] = useState(prnm);
+  const [productdeadline, setProductdeadline] = useState(deadline);
 
-export const setScannedData = (data) => {
-    scannedData = data;
-}
+  const handleProductNumberChange = (text) => {
+    setProductNumber(text);
+  };
+  const handleProductNameChange = (text) => {
+    setProductName(text);
+  }
+  const handleProductDeadlineChange = (text) => {
+    setProductdeadline(text);
+  }
 
-export const getScannedData = () => {
-    return scannedData;
-}
-
-export function Import ({route}) {
-    const{ data } = route.params;
-
-return (
-    <View style = {StyleSheet.container}>
-        <Text> product number: {getScannedData}</Text>
-        <View style = {StyleSheet.buttonContainer}>
-            <Text>Input product number</Text>
-            <TextInput
-                keyboardType="numeric"
-                style = {data}
-                placeholder = '0'
-                onChangeText={getScannedData}/>
-        </View>
+  return (
+    <View style={styles.container}>
+      <Text>Product number: {data}</Text>
+      <View style={styles.buttonContainer}>
+        <Text>Input product number</Text>
+        <TextInput
+          keyboardType="numeric"
+          style={styles.input}
+          placeholder="0"
+          value={productNumber} // Bind the input value to the productNumber state
+          onChangeText={handleProductNumberChange} // Update the productNumber state
+        />
+        <TextInput
+          style = {styles.input}
+          placeholder="0"
+          value={productName}
+          onChangeText={handleProductNameChange}
+          />
+        <TextInput
+          style = {styles.input}
+          placeholder="0"
+          value= {productdeadline}
+          onChangeText={handleProductDeadlineChange}
+          />
+      </View>
     </View>
-)}
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 5,
+  },
+});
 
 export default Import;
--- barcoding/Scan/Scan.js
+++ barcoding/Scan/Scan.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Audio } from 'expo-av';
 
-const ScannerScreen = ({ navigation }) => {
+const REST_API_KEY = 'f98ace30dbf14b9bbcbc';
+const OPENAPI_URL = 'http://openapi.foodsafetykorea.go.kr/api/';
+
+export default function Scan({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [scannedData, setScannedDataLocally] = useState('');
+  const [scannedprnm, setScannedprnm] = useState('');
+  const [scanneddeadline, setScanneddeadline] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -16,29 +22,70 @@ const ScannerScreen = ({ navigation }) => {
 
   useEffect(() => {
     if (scannedData) {
-      navigation.navigate('Import', { data: scannedData }); // ½ºÄµµÈ µ¥ÀÌÅÍ¸¦ ´ÙÀ½ ½ºÅ©¸°À¸·Î Àü´Þ
+      navigation.navigate('Import', { data: scannedData, prnm, deadline }); // ½ºÄµµÈ µ¥ÀÌÅÍ¸¦ ´ÙÀ½ ½ºÅ©¸°À¸·Î Àü´Þ
     }
-  }, [scannedData, navigation]);
+  }, [scannedData, scannedprnm, scanneddeadline, navigation]);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = async ({ data }) => {
     setScanned(true);
     setScannedDataLocally(data);
-
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('../assets/Sound/barcode.mp3'));
+      await soundObject.playAsync();
+    } catch (error) {
+      console.error('?‚¬?š´?“œ ë¡œë”©ì¤? ?—?Ÿ¬:', error);
+    }
+    try {
+      const result = await getdata(data);
+  
+      if (result.error) {
+        alert(`Bar code data ${data} has been scanned, but there was an error: ${result.error}`);
+      } else {
+        alert(`ë°”ì½”?“œë²ˆí˜¸ : ${data} \n? œ?’ˆëª? : ${result.prnm}\n?œ ?†µê¸°í•œ : ${result.deadline}`);
+        setScannedprnm(result.prnm);
+        setScanneddeadline(result.deadline);
+      }
+    } catch (error) {
+      console.error('?‹ë³„ë˜ì§? ?•Š?Š” ë°”ì½”?“œ ë²ˆí˜¸', error);
+    }
+    
   };
+  
+  // API key -> f98ace30dbf14b9bbcbc
+  const getdata = async (qrvalue) => {
+    const response = await fetch(
+        OPENAPI_URL + REST_API_KEY + '/C005/json/1/5/BAR_CD=' +
+        qrvalue,
+      {
+        method: 'GET',
+      }
+    );
+  
+    if (response.status === 200) {
+      const responseJson = await response.json();
+      const prnm = responseJson.C005.row[0].PRDLST_NM;
+      const deadline = responseJson.C005.row[0].POG_DAYCNT;
+      return { prnm, deadline };
+    } else {
+      return { error: 'There was an error' };
+    }
+  };
+  
+  
+  if (hasPermission === null) {
+    return <Text>Requesting camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      {hasPermission === null ? (
-        <Text>Requesting camera permission</Text>
-      ) : hasPermission === false ? (
-        <Text>No access to camera</Text>
-      ) : (
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-        />
-      )}
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
       {scanned && (
         <View style={styles.scanAgain}>
           <Text>Scan again?</Text>
@@ -47,15 +94,16 @@ const ScannerScreen = ({ navigation }) => {
           </Text>
         </View>
       )}
+
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
   scanAgain: {
     flexDirection: 'row',
@@ -68,5 +116,3 @@ const styles = StyleSheet.create({
     color: 'blue',
   },
 });
-
-export default ScannerScreen;
